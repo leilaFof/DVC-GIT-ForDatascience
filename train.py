1f371179ffa5945451398673af8cf5be72222ba7
leilaFof <kokofof142@gmail.com>
2024-03-29 11:15:16 +0000

    add function say_bay

--- train.py
+++ train.py
@@ -1,7 +1,15 @@
 def say_hello(name):
     print("hello {} ,the data scientist".format(name))
+
 def greetings(name):
     print("This is a gretting from {} ".format(name))
+def say_bay(name):
+    print(f'Good bay {name}')
+    
+
+
+
 
 say_hello("Leila")
 greetings("Makoko")
+say_bay('Aicah')
