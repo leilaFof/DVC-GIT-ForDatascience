5ec47dce2d92847f69fee33c61082fe101f3cdf5
leilaFof <kokofof142@gmail.com>
2024-03-29 10:51:07 +0000

    Modified train File add a nex function

--- train.py
+++ train.py
@@ -1,4 +1,7 @@
 def say_hello(name):
     print("hello {} ,the data scientist".format(name))
+def greetings(name):
+    print("This is a gretting from {} ".format(name))
 
-say_hello("leila")
+say_hello("Leila")
+greetings("Makoko")
